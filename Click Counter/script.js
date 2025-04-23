e3ae00fc6f8b6c77765c09bb3a0ef1cc9663b691
Tanvir <tanvird556@gmail.com>
2025-04-24 01:59:41 +0600

    done project one

--- Click Counter/script.js
+++ Click Counter/script.js
@@ -3,7 +3,40 @@ const btnD = document.getElementById("btnD");
 const btnR = document.getElementById("btnR");
 const numCtr = document.getElementById("numCtr");
 
+numCtr.innerText = localStorage.getItem("counts") || 0;
 
-document.getElementById("btnI").addEventListener("click", btnI) {
-    document.getElementById("numCtr").innerText = Number(document.getElementById("numCtr").innerText) +1;
-};
+function cnumColor() {
+  if (Number(numCtr.innerText) == 0) {
+    numCtr.style.color = "red";
+  } else {
+    numCtr.style.color = "white";
+  }
+  if (Number(numCtr.innerText) < 0) {
+    numCtr.style.color = "black";
+  }
+}
+
+btnI.addEventListener("click", () => {
+  numCtr.innerText = Number(numCtr.innerText) + 1;
+  localStorage.setItem("counts", numCtr.innerText);
+  cnumColor();
+});
+
+// To make it dubble click just change click to dbclick
+btnD.addEventListener("click", () => {
+  // Add a feature where double-clicking the button decreases the count by 1 (but don’t let it go below 0) //
+  // if (Number(numCtr.innerText) > 0) {
+  //   numCtr.innerText = Number(numCtr.innerText) - 1;
+  //   localStorage.setItem("counts", numCtr.innerText);
+  // }
+
+  numCtr.innerText = Number(numCtr.innerText) - 1;
+  localStorage.setItem("counts", numCtr.innerText);
+  cnumColor();
+});
+
+btnR.addEventListener("click", () => {
+  numCtr.innerText = 0;
+  localStorage.setItem("counts", numCtr.innerText);
+  cnumColor();
+});
