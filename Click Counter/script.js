9de58683bb114fec75518db9588a7f3f5b456ba0
Tanvir <tanvird556@gmail.com>
2025-04-24 01:34:23 +0600

    just made some progress.

---
+++ Click Counter/script.js
@@ -0,0 +1,9 @@
+const btnI = document.getElementById("btnI");
+const btnD = document.getElementById("btnD");
+const btnR = document.getElementById("btnR");
+const numCtr = document.getElementById("numCtr");
+
+
+document.getElementById("btnI").addEventListener("click", btnI) {
+    document.getElementById("numCtr").innerText = Number(document.getElementById("numCtr").innerText) +1;
+};
